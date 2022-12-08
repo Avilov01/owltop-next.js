5eda70ad60b2c8973699a9f875922d123416ad9a
Avilov01 <Avilov01@gmail.com>
2022-12-08 23:29:50 +0300

    add title component

---
+++ components/Title/Title.tsx
@@ -0,0 +1,21 @@
+import { ReactNode } from 'react';
+
+import styles from './Title.module.css';
+
+interface TitleProp {
+  variant: 'h1' | 'h2' | 'h3';
+  children: ReactNode;
+}
+
+export const Title = ({ variant, children }: TitleProp): JSX.Element => {
+  switch (variant) {
+    case 'h1':
+      return <h1 className={styles.h1}>{children}</h1>;
+    case 'h2':
+      return <h2 className={styles.h2}>{children}</h2>;
+    case 'h3':
+      return <h3 className={styles.h3}>{children}</h3>;
+    default:
+      return <></>;
+  }
+};
