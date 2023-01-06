a83c295a371ea5bf26b40e0a12bddeaf8bf8bac3
Avilov01 <Avilov01@gmail.com>
2023-01-06 23:46:30 +0300

    create context

--- layout/index.tsx
+++ layout/index.tsx
@@ -1,10 +1,10 @@
 import { FC, ReactNode } from 'react';
 
+import { AppContextProvider, IAppContext } from '../context/app.context';
 import { Footer } from './Footer';
 import { Header } from './Header';
-import { Sidebar } from './Sidebar';
 import styles from './Layout.module.css';
-
+import { Sidebar } from './Sidebar';
 
 type LayoutProps = {
   children: ReactNode;
@@ -13,12 +13,24 @@ type LayoutProps = {
 export const Layout: FC<LayoutProps> = ({ children }) => {
   return (
     <div className={styles.wrapper}>
-      <Header className={styles.header}/>
-        <Sidebar className={styles.sidebar}/>
-        <div className={styles.content}>
-          {children}
-        </div>
-      <Footer className={styles.footer}/>
+      <Header className={styles.header} />
+      <Sidebar className={styles.sidebar} />
+      <div className={styles.content}>
+        {children}
+      </div>
+      <Footer className={styles.footer} />
     </div>
   );
+};
+
+export const withLayout = <T extends Record<string, unknown> & IAppContext>(Component: FC<T>) => {
+  return function withLayoutComponent(props: T): JSX.Element {
+    return (
+      <AppContextProvider menu={props.menu} firstCategory={props.firstCategory}>
+        <Layout>
+          <Component {...props} />
+        </Layout>
+      </AppContextProvider>
+    );
+  };
 };
