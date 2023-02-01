448841d0b989fdcbd69b564c2c60956a7a371ab5
Avilov01 <Avilov01@gmail.com>
2023-02-02 00:15:41 +0300

    add home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,37 @@
-import styles from '../styles/Home.module.css';
-import { Title } from '../components';
+import axios from 'axios';
+import { GetStaticProps } from 'next';
 
-export default function Home(): JSX.Element {
+import { Title } from '../components';
+import { MenuItem } from '../interfaces/menu.interface';
+import { TopLevelCategory } from '../interfaces/page.interface';
+import { withLayout } from '../layout';
+
+interface HomeProps {
+  menu: MenuItem[];
+  firstCategory: TopLevelCategory;
+}
+
+function Home({ menu, firstCategory }: HomeProps): JSX.Element {
   return (
-    <div className={styles.container}>
-      <Title variant='h1'>
-        Курсы по Photoshop
-      </Title>
-    </div>
+    <Title variant='h1'>
+      Home page
+    </Title>
   );
 }
+
+export default withLayout(Home);
+
+export const getStaticProps: GetStaticProps<HomeProps> = async () => {
+  const firstCategory = 0;
+
+  const { data: menu } = await axios.post<MenuItem[]>(`${process.env.NEXT_PUBLIC_DOMAIN}/api/top-page/find`, {
+    firstCategory
+  });
+
+  return {
+    props: {
+      menu,
+      firstCategory
+    }
+  };
+};
