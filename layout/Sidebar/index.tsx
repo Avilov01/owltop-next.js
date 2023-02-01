5cad38149d4440e058812ba5e5747dec2955b6ff
Avilov01 <Avilov01@gmail.com>
2023-02-02 00:14:29 +0300

    edit sidebar

--- layout/Sidebar/index.tsx
+++ layout/Sidebar/index.tsx
@@ -1,12 +1,19 @@
 import { DetailedHTMLProps, FC, HTMLAttributes } from 'react';
 
+import cn from 'classnames';
+
+import Logo from '../../assets/icons/logo.svg';
+import { SearchInput } from '../../components/SearchInput';
 import { Menu } from '../Menu';
+import styles from './Sidebar.module.css';
 
 interface SidebarProps extends DetailedHTMLProps<HTMLAttributes<HTMLDivElement>, HTMLDivElement> {}
 
-export const Sidebar: FC<SidebarProps> = ({...props}) => {
+export const Sidebar: FC<SidebarProps> = ({ className, ...props}) => {
   return (
-    <div {...props}>
+    <div className={cn(className, styles.sidebar)} {...props}>
+      <Logo className={styles.logo} />
+      <SearchInput />
       <Menu />
     </div>
   );
